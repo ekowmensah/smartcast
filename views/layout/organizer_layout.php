aca99e22e58fbb333b129f27e34c8af579e990a9
ekowmensah <ekowme@gmail.com>
2025-10-20 03:37:10 +0000

    git push

--- views/layout/organizer_layout.php
+++ views/layout/organizer_layout.php
@@ -472,5 +472,19 @@
     })();
     </script>
     <!--End of Tawk.to Script-->
+
+
+      <!--Google Analytics-->
+    <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4T3JY6872R"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-4T3JY6872R');
+        </script>
+
+        
 </body>
 </html>
